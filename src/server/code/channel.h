3512ce43c93cdfbafe5746afb20aeb8a4ef30cc2
sm222 <soleil.maraska@gmail.com>
2025-12-08 16:49:15 -0500

    working on channel

--- src/server/code/channel.h
+++ src/server/code/channel.h
@@ -6,14 +6,37 @@
 
 
 typedef enum {
-  cs_open = 1,
-
+  cs_open           = 1,
+  cs_user_message   = 2,
+  cs_user_can_join  = 4,
+  cs_user_auto_join = 8,
 } channel_status;
+
+typedef enum {
+  txt,
+  img,
+  audio,
+  file,
+} msg_type;
+
+typedef struct s_msg {
+  msg_type     tag;
+  size_t       size;
+  void*        data;
+} t_msg;
+
+typedef struct s_msg_block {
+  struct s_msg_block* next;
+  t_msg  msgs[100];
+} t_msg_block;
 
 typedef struct s_channel {
   struct s_channel* next;
+  struct s_channel* prev;
+  const char*       name;
   int32_t           status;
-  
+  size_t            maxUser;
+
 } channel;
 
 #endif
